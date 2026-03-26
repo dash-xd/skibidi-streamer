7889fbf25dc1075e8430d38cc32e3bc81505d900
dev@dashxd <125025267+dash-xd@users.noreply.github.com>
2026-03-26 00:10:09 -0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,19 @@
 #!/usr/bin/env bash
 set -e
+
+# Source nix.sh
 if [ -f "$HOME/.nix-profile/etc/profile.d/nix.sh" ]; then
     . "$HOME/.nix-profile/etc/profile.d/nix.sh"
 else
     echo "ERROR: nix.sh not found!"
     exit 1
 fi
+
+# Explicitly export PATH just in case
+export PATH="$HOME/.nix-profile/bin:$PATH"
+
 CMD=${ENTRYPOINT:-"wrangler dev"}
 echo "Entering Nix dev shell and running: $CMD"
-nix develop . --command $CMD
+
+# Run flake development shell
+exec nix develop . --command "$CMD"
