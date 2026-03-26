0db57ede81fc711b53269dc2749798010d9dd4fe
dev@dashxd <125025267+dash-xd@users.noreply.github.com>
2026-03-25 23:25:12 -0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 set -e
+. $HOME/.nix-profile/etc/profile.d/nix.sh
 CMD=${ENTRYPOINT:-"wrangler dev"}
 echo "Entering Nix dev shell and running: $CMD"
 exec nix develop . --command $CMD
